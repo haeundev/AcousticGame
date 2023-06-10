b9429832759a60270219a89edc55d6a6b4b44872
haeundev <haeundev@gmail.com>
2023-06-11 02:14:34 +0900

    task infos asset

---
+++ AsylumAcousticGame/Assets/Data/Table/Script/TaskInfos.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+namespace Proto.Data
+{
+    public class TaskInfos : ScriptableObject
+    {
+        public List<TaskInfo> Values;
+    }
+}
+
