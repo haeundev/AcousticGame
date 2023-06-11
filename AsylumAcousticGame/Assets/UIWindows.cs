d3275da180380585d9016c54a085312b5c02c3f3
haeundev <haeundev@gmail.com>
2023-06-11 19:46:42 +0900

    ui windows

--- AsylumAcousticGame/Assets/UIWindows.cs
+++ AsylumAcousticGame/Assets/UIWindows.cs
@@ -1,18 +1,25 @@
+using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
 public class UIWindows : MonoBehaviour
 {
     public static UIWindows Instance;
+    private static List<UI_Window> _windows;
 
     private void Awake()
     {
         Instance = this;
+        _windows = GetComponentsInChildren<UI_Window>().ToList();
+
+        foreach (var window in _windows)
+        {
+            window.gameObject.SetActive(false);
+        }
     }
 
     public static UI_Window GetWindow(int id)
     {
-        var windowComps = Instance.gameObject.GetComponentsInChildren<UI_Window>();
-        return windowComps.FirstOrDefault(windowComp => windowComp.ID == id);
+        return _windows.FirstOrDefault(windowComp => windowComp.ID == id);
     }
 }
